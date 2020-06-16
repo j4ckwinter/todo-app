ddf09e31375d39d9ef7fcf7c774b1399c72fcc43
Jack Winter <jackwinter.10@gmail.com>
2020-06-16 10:15:48 +0100

    Dependency injection

--- src/app/todo/todo.service.ts
+++ src/app/todo/todo.service.ts
@@ -9,7 +9,9 @@ export class TodoService {
   todoSelected = new EventEmitter<TodoItem>();
   private todoItems: TodoItem[] = [
     new TodoItem(1, "Reply to HR", "Reply to HR email about benefits", 0.5, 1, "Admin", "11/06/2020", "12/06/2020", 3),
-    new TodoItem(2, "Do HR training", "Finish HR assigned training course", 1, 1, "Training", "10/06/2020", "15/06/2020", 3)
+    new TodoItem(2, "Do HR training", "Finish HR assigned training course", 1, 1, "Training", "10/06/2020", "15/06/2020", 3),
+    new TodoItem(3, "Keystore password encryption", "Investigate a way to encrypt the keystore password", 5, 1, "Development", "23/04/2020", "09/08/2020", 4)
+
   ];
 
   constructor() { }
